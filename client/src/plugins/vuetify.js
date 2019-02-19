031f731f985d46cdf1e77d89788896d858191933
Oscar Sandoval <apollo.sandoval@gmail.com>
2019-02-19 13:02:58 -0700

    added landing page with styling

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify, { colors } from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 
+// TODO: Finalize theme colors
 Vue.use(Vuetify, {
   iconfont: 'md',
   theme: {
--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -1,11 +1,16 @@
 <template>
-  <v-container fluid fill-height>
+  <v-container fluid fill-height pa-0>
     <!-- TODO: find a sick landing page photo -->
+    <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      >
+        <v-container fluid fill-height>
+          <v-layout align-center justify-center>
+            <v-flex xs12 sm8 class="text-xs-center">
+              <span class="display-4 white--text font-weight-light font-italic">NutriTrack</span>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-img>
   </v-container>
 </template>
-
-<script>
-export default {
-  
-}
-</script>
